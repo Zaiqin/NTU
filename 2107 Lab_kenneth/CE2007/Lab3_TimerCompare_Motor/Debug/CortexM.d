fe99ebcfcac720c541f35697e367e0b064a7273b
Zaiqin <phuazaiqin@gmail.com>
2023-10-19 14:29:10 +0800

    .

---
+++ 2107 Lab_kenneth/CE2007/Lab3_TimerCompare_Motor/Debug/CortexM.d
@@ -0,0 +1,7 @@
+# FIXED
+
+CortexM.obj: C:/Users/lowz0021/Desktop/2107\ Lab/CE2007/inc/CortexM.c
+CortexM.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+
+C:/Users/lowz0021/Desktop/2107\ Lab/CE2007/inc/CortexM.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
